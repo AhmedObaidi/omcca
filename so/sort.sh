081b374b5f56d06904ecdcf993a8f20dacf727c4
Ahmed Obaidi <ahmad.alobaidy@gmail.com>
2014-02-11 19:47:54 +0300

    Delete sort.sh

--- so/sort.sh
+++
@@ -1,7 +0,0 @@
-for filename in *.*; do
-   if [ "${filename: -4}" != ".nii" ]; then
-      dir_name=${filename:0:2};
-      mkdir -p $dir_name
-      mv -i $filename $dir_name/${filename}
-   fi
-done
